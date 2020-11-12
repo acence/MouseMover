15fde7e80acf0233d828856b49423e08efc842f2
Aleksandar Trajkov <aleksandar.trajkov@allocatesoftware.com>
2020-11-12 10:41:54 +0100

    Add project files.

---
+++ MouseMover/MouseMover.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MouseMover
+{
+    partial class MouseMover
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MouseMover));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timMove = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 26);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(186, 29);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(12, 61);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(186, 29);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timMove
+            // 
+            this.timMove.Enabled = true;
+            this.timMove.Interval = 30000;
+            this.timMove.Tick += new System.EventHandler(this.timMove_Tick);
+            // 
+            // MouseMover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 114);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MouseMover";
+            this.Text = "Mouse Mover";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timMove;
+    }
+}
+
---
+++ MouseMover/MouseMover.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseMover
+{
+    public partial class MouseMover : Form
+    {
+        private bool move = false;
+        private Random randomizer = new Random();
+        public MouseMover()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            move = true;   
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            move = false;
+        }
+
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private void timMove_Tick(object sender, EventArgs e)
+        {
+            if (move)
+            {
+                var newPosX = randomizer.Next(0, 1920);
+                var newPosY = randomizer.Next(0, 1080);
+                this.Cursor = new Cursor(Cursor.Current.Handle);
+                Cursor.Position = new Point(newPosX, newPosY);
+                SendKeys.Send(chars[randomizer.Next(0, 35)].ToString());
+            }
+        }
+    }
+}
