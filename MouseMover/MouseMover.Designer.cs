d73123bb7846c39ad94907f513b8ea247a2d9c06
Aleksandar Trajkov <aleksandar.trajkov@allocatesoftware.com>
2020-11-26 17:09:26 +0100

    added shutdown option

--- MouseMover/MouseMover.Designer.cs
+++ MouseMover/MouseMover.Designer.cs
@@ -33,6 +33,7 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.timMove = new System.Windows.Forms.Timer(this.components);
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // btnStart
@@ -61,11 +62,23 @@
             this.timMove.Interval = 30000;
             this.timMove.Tick += new System.EventHandler(this.timMove_Tick);
             // 
+            // timePicker
+            // 
+            this.timePicker.CustomFormat = "HH:mm";
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.timePicker.Location = new System.Drawing.Point(12, 112);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.ShowUpDown = true;
+            this.timePicker.Size = new System.Drawing.Size(186, 23);
+            this.timePicker.TabIndex = 1;
+            this.timePicker.Value = new System.DateTime(2020, 11, 26, 16, 46, 47, 0);
+            // 
             // MouseMover
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(210, 114);
+            this.ClientSize = new System.Drawing.Size(210, 149);
+            this.Controls.Add(this.timePicker);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -80,6 +93,7 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Timer timMove;
+        private System.Windows.Forms.DateTimePicker timePicker;
     }
 }
 
--- MouseMover/MouseMover.cs
+++ MouseMover/MouseMover.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,33 +13,55 @@ namespace MouseMover
 {
     public partial class MouseMover : Form
     {
+        Thread shutdownThread;
         private bool move = false;
-        private Random randomizer = new Random();
+        private static Random randomizer = new Random();
         public MouseMover()
         {
             InitializeComponent();
+            timePicker.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 17, 00, 00);
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            move = true;   
+            timMove.Start();
+            if (shutdownThread != null && (shutdownThread.ThreadState == ThreadState.Running || shutdownThread.ThreadState == ThreadState.WaitSleepJoin))
+            {
+                shutdownThread.Interrupt();
+            }
+            shutdownThread = new Thread(new ParameterizedThreadStart(ShutdownWait));
+            shutdownThread.Start(timePicker.Value);
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
-            move = false;
+            timMove.Stop();
+            if (shutdownThread != null && (shutdownThread.ThreadState == ThreadState.Running || shutdownThread.ThreadState == ThreadState.WaitSleepJoin))
+            {
+                shutdownThread.Interrupt();
+            }
         }
 
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         private void timMove_Tick(object sender, EventArgs e)
         {
-            if (move)
+            var newPosX = randomizer.Next(0, 1920);
+            var newPosY = randomizer.Next(0, 1080);
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(newPosX, newPosY);
+            SendKeys.Send(chars[randomizer.Next(0, 35)].ToString());
+        }
+
+        static void ShutdownWait(object time)
+        {
+            try
             {
-                var newPosX = randomizer.Next(0, 1920);
-                var newPosY = randomizer.Next(0, 1080);
-                this.Cursor = new Cursor(Cursor.Current.Handle);
-                Cursor.Position = new Point(newPosX, newPosY);
-                SendKeys.Send(chars[randomizer.Next(0, 35)].ToString());
+                var shutdownTime = (DateTime)time;
+                var diff = Convert.ToInt32((DateTime.Now - shutdownTime).TotalMilliseconds) + randomizer.Next(0, 5*60*1000);
+                Thread.Sleep(diff);
+                System.Diagnostics.Process.Start("CMD.exe", "shutdown -s -t 0");
+            }
+            catch {
             }
         }
     }
